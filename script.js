e5b78747331e87a9b3ed2f0b05944468e08c4a64
Joel Singh <joelsingh788@gmail.com>
2022-09-23 18:00:49 -0400

    Add checkForWinner function

--- script.js
+++ script.js
@@ -25,7 +25,29 @@
         }
       });
     };
-    return { drawBoard };
+
+    const checkForWinner = (symbol) => {
+      const check = (index) => gameBoard[index] === symbol;
+      const firstColCheck = check(0) && check(3) && check(6);
+      const secondColCheck = check(1) && check(4) && check(7);
+      const thirdColCheck = check(2) && check(5) && check(8);
+      const firstRowCheck = check(0) && check(1) && check(2);
+      const secondRowCheck = check(3) && check(4) && check(5);
+      const thirdRowCheck = check(6) && check(7) && check(8);
+      const firstDiagCheck = check(0) && check(4) && check(8);
+      const secondDiagCheck = check(2) && check(4) && check(6);
+      return (
+        firstColCheck ||
+        secondColCheck ||
+        thirdColCheck ||
+        firstRowCheck ||
+        secondRowCheck ||
+        thirdRowCheck ||
+        firstDiagCheck ||
+        secondDiagCheck
+      );
+    };
+    return { drawBoard, checkForWinner };
   })();
 
   board.drawBoard();
