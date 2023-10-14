53a6b00f302cc0e67a3e5b1344cf71081c30abb9
Joel Singh <joelsingh788@gmail.com>
2023-10-13 21:17:33 -0400

    Move Board into its own file

---
+++ src/ts/Board.ts
@@ -0,0 +1,68 @@
+export default function Board() {
+  const gameBoard = [
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+  ];
+
+  const getCell = (index) => {
+    return document.querySelector(`.cell[data-cell-index='${index}']`);
+  };
+
+  const isFilled = () => !gameBoard.includes("empty");
+
+  const drawBoard = () => {
+    gameBoard.forEach((marking, index) => {
+      if (marking === "o") {
+        getCell(index).classList.add("o");
+      } else if (marking === "x") {
+        getCell(index).classList.add("x");
+      } else if (marking === "empty") {
+        getCell(index).classList.remove("x");
+        getCell(index).classList.remove("o");
+      }
+    });
+  };
+
+  const isWinner = (symbol) => {
+    const check = (index) => gameBoard[index] === symbol;
+    const firstColCheck = check(0) && check(3) && check(6);
+    const secondColCheck = check(1) && check(4) && check(7);
+    const thirdColCheck = check(2) && check(5) && check(8);
+    const firstRowCheck = check(0) && check(1) && check(2);
+    const secondRowCheck = check(3) && check(4) && check(5);
+    const thirdRowCheck = check(6) && check(7) && check(8);
+    const firstDiagCheck = check(0) && check(4) && check(8);
+    const secondDiagCheck = check(2) && check(4) && check(6);
+    return (
+      firstColCheck ||
+      secondColCheck ||
+      thirdColCheck ||
+      firstRowCheck ||
+      secondRowCheck ||
+      thirdRowCheck ||
+      firstDiagCheck ||
+      secondDiagCheck
+    );
+  };
+
+  const editBoard = (index, symbol) => {
+    gameBoard[index] = symbol;
+    drawBoard();
+  };
+
+  const reset = () => {
+    gameBoard.fill("empty");
+    drawBoard();
+  };
+
+  return { isWinner, isFilled, editBoard, reset };
+};
+
+export type BoardType = ReturnType<typeof Board>;
