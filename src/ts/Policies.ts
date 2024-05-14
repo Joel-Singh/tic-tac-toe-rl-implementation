bacac4d945e5702419e0a6aad4b2e4014d0883e5
Joel Singh <joelsingh788@gmail.com>
2024-05-14 17:29:53 -0400

    Generate all full tic tac toe boards

--- src/ts/Policies.ts
+++ src/ts/Policies.ts
@@ -10,18 +10,44 @@ function Policy(state: BoardState, value: number) {
 
 type Policy = ReturnType<typeof Policy>
 
+// assume Reino always goes second and is x
 export function createPolicies() {
   const policies: Policy[] = [];
-  for (let i = 1; i <= 8; i++) {
-    const boardState = createEmptyBoardState();
-    boardState[0] = 'o';
 
-    boardState[i] = 'x';
-
-    const policy = Policy(boardState, 0);
-    policies.push(policy);
+  function addPolicy(turnSymbol: possibleBoardStates, currentState: BoardState) {
+    if (isFull(currentState)) {
+      policies.push(Policy(currentState, 0));
+    } else {
+      for (let i = 0; i < 9; i++) {
+        if (currentState[i] === 'empty') {
+          currentState[i] = turnSymbol;
+          addPolicy(turnSymbol === 'x' ? 'o' : 'x', createCopy(currentState));
+          currentState[i] = 'empty';
+        }
+      }
+    }
   }
-  return policies;
+
+  addPolicy('o', createEmptyBoardState());
+  return policies
+
+  function isFull(boardState: BoardState) {
+    return !boardState.includes('empty');
+  }
+
+  function createCopy(boardState: BoardState): BoardState {
+    return [
+      boardState[0],
+      boardState[1],
+      boardState[2],
+      boardState[3],
+      boardState[4],
+      boardState[5],
+      boardState[6],
+      boardState[7],
+      boardState[8],
+    ]
+  }
 }
 
 function createEmptyBoardState(): BoardState {
