f7d21e18b4c9713f8585a14e0b974165de6fd105
Joel Singh <joelsingh788@gmail.com>
2024-05-19 16:57:13 -0400

    Policies are updated

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -15,16 +15,26 @@ export default function Game(oPlayer: PlayerType, xPlayer: PlayerType, board: Bo
     currentPlayerTurn.textContent = ''
   }
 
+  let originalPolicy = createAllPossiblePolicies();
+
+  // reino is x player
   const start = async () => {
-    let currentPolicy = createAllPossiblePolicies();
+    let currentPolicy = originalPolicy;
     while (true) {
       await playerTurn(oPlayer, currentPolicy)
+
+      let previousPolicy = currentPolicy;
       currentPolicy = currentPolicy.possibleMoves[board.getLastEditedIndex()];
+      updatePolicy(currentPolicy, previousPolicy);
+
       if (isGameDone()) {
         break;
       }
       await playerTurn(xPlayer, currentPolicy)
+      previousPolicy = currentPolicy;
       currentPolicy = currentPolicy.possibleMoves[board.getLastEditedIndex()];
+      updatePolicy(currentPolicy, previousPolicy);
+
       if (isGameDone()) {
         break;
       }
@@ -41,4 +51,10 @@ export default function Game(oPlayer: PlayerType, xPlayer: PlayerType, board: Bo
     (document.querySelector("#start-game") as HTMLElement).style.opacity = "1";
   };
   return { start };
+
+  // equation on page 10 of rl by barto and sutton second edition: V(S_t) <- V(S_t) + a[V(S_t+1) - V(S_t)]
+  function updatePolicy(currentPolicy: Policy, previousPolicy: Policy) {
+    const stepSizeParameter = 1;
+    previousPolicy.value = previousPolicy.value + stepSizeParameter*(currentPolicy.value - previousPolicy.value);
+  }
 };
